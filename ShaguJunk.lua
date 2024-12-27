9a1a32db20db4dcab33acc529ef0d5ce43945ea1
Distrattos <development@distrattos.com>
2024-12-27 11:53:24 +0100

    autodelete: added dungeon list for items only delete while in instance

--- ShaguJunk.lua
+++ ShaguJunk.lua
@@ -15,6 +15,7 @@ end
 do -- config
   ShaguJunk_vendor = ShaguJunk_vendor or {}
   ShaguJunk_delete = ShaguJunk_delete or {}
+  ShaguJunk_dungeon = ShaguJunk_dungeon or {}
 
   SLASH_SHAGUJUNK1, SLASH_SHAGUJUNK2, SLASH_SHAGUJUNK3 = "/sjunk", "/junk", "/sj"
   SlashCmdList["SHAGUJUNK"] = function(message)
@@ -63,10 +64,30 @@ do -- config
       table.insert(ShaguJunk_delete, string.lower(addstring))
       DEFAULT_CHAT_FRAME:AddMessage("=> adding |cff33ffcc".. addstring .."|r to your delete list")
 
+    -- add dungeon entry
+    elseif (commandlist[1] == "dungeon" or commandlist[1] == "instance") then
+      local addstring = table.concat(commandlist," ",2)
+      if addstring == "" then return end
+
+      -- support item links
+      local _, _, itemLink = string.find(addstring, "(item:%d+:%d+:%d+:%d+)")
+      local itemName = itemLink and GetItemInfo(itemLink)
+
+      addstring = itemName or addstring
+
+      if tContains(ShaguJunk_dungeon, string.lower(addstring)) then
+        DEFAULT_CHAT_FRAME:AddMessage("=> Dungeon delete list already contains " .. addstring)
+        return
+      end
+
+      table.insert(ShaguJunk_dungeon, string.lower(addstring))
+      DEFAULT_CHAT_FRAME:AddMessage("=> adding |cff33ffcc".. addstring .."|r to your dungeon delete list")
+
     -- remove entry
     elseif commandlist[1] == "rm" then
       local vendor = tonumber(commandlist[2])
       local delete = tonumber(commandlist[2]) - table.getn(ShaguJunk_vendor)
+      local dungeon = tonumber(commandlist[2]) - table.getn(ShaguJunk_vendor) - table.getn(ShaguJunk_delete)
 
       if ShaguJunk_vendor[vendor] then
         DEFAULT_CHAT_FRAME:AddMessage("=> Removing entry " .. commandlist[2]
@@ -80,27 +101,48 @@ do -- config
           .. " (" .. ShaguJunk_delete[delete]
           .. ") from your deletion list")
         table.remove(ShaguJunk_delete, delete)
+
+      elseif ShaguJunk_dungeon[dungeon] then
+        DEFAULT_CHAT_FRAME:AddMessage("=> Removing entry " .. commandlist[2]
+                .. " (" .. ShaguJunk_dungeon[dungeon]
+                .. ") from your dungeon deletion list")
+        table.remove(ShaguJunk_dungeon, dungeon)
       end
     elseif commandlist[1] == "ls" then
       local addstring = table.concat(commandlist," ",2)
       local printID = 0
+      local tempID = 0
       DEFAULT_CHAT_FRAME:AddMessage("|cff33ee33Vendor Items:")
       for id, hl in pairs(ShaguJunk_vendor) do
         if string.find(hl, addstring) then
           DEFAULT_CHAT_FRAME:AddMessage(" |r[|cff33ee33"..id.."|r] "..hl)
         end
-        printID = id
+        tempID = id
       end
+      printID = printID + tempID; tempID = 0
+
       DEFAULT_CHAT_FRAME:AddMessage("|cffaa3333Delete Items:")
       for id, hl in pairs(ShaguJunk_delete) do
         if string.find(hl, addstring) then
           DEFAULT_CHAT_FRAME:AddMessage(" |r[|cffee3333"..id+printID.."|r] "..hl)
         end
+        tempID = id
       end
+      printID = printID + tempID; tempID = 0
+
+      DEFAULT_CHAT_FRAME:AddMessage("|cffaa3333Dungeon Items:")
+      for id, hl in pairs(ShaguJunk_dungeon) do
+        if string.find(hl, addstring) then
+          DEFAULT_CHAT_FRAME:AddMessage(" |r[|cffee3333"..id+printID.."|r] "..hl)
+        end
+        tempID = id
+      end
+
     else
       DEFAULT_CHAT_FRAME:AddMessage("ShaguJunk Usage:")
       DEFAULT_CHAT_FRAME:AddMessage("|cffaaffdd/sjunk vendor Fel Iron Blood Ring|cffaaaaaa - |rAutomatically vendors Fel Iron Rings")
       DEFAULT_CHAT_FRAME:AddMessage("|cffaaffdd/sjunk delete Light Hide|cffaaaaaa - |rAutomatically deletes Light Hide")
+      DEFAULT_CHAT_FRAME:AddMessage("|cffaaffdd/sjunk dungeon Light Hide|cffaaaaaa - |rAutomatically deletes Light Hide when inside dungeon")
       DEFAULT_CHAT_FRAME:AddMessage("|cffaaffdd/sjunk rm 3|cffaaaaaa - |rRemoves entry '3' of your list")
       DEFAULT_CHAT_FRAME:AddMessage("|cffaaffdd/sjunk ls|cffaaaaaa - |rDisplays your current list")
       DEFAULT_CHAT_FRAME:AddMessage("|cffaaffdd/sjunk ls <text>|cffaaaaaa - |rSearch your current list for text")
@@ -171,6 +213,8 @@ do -- autodelete
     -- throttle to to one item per .1 second
     if ( this.tick or 1) > GetTime() then return else this.tick = GetTime() + .1 end
 
+    inInstance, instanceType = IsInInstance()
+
     -- iterate through bag
     for bag = 0, 4, 1 do
       for slot = 1, GetContainerNumSlots(bag), 1 do
@@ -180,8 +224,8 @@ do -- autodelete
         name = name and string.lower(name)
 
         if name then
-          for i, vendor in pairs(ShaguJunk_delete) do
-            if name == vendor then
+          for i, delete in pairs(ShaguJunk_delete) do
+            if name == delete then
               -- clear cursor and delete the item
               ClearCursor()
               PickupContainerItem(bag, slot)
@@ -190,6 +234,20 @@ do -- autodelete
               return
             end
           end
+
+          if inInstance then
+            for i, dungeon in pairs(ShaguJunk_dungeon) do
+              if name == dungeon then
+                -- clear cursor and delete the item
+                ClearCursor()
+                PickupContainerItem(bag, slot)
+                DeleteCursorItem()
+                -- continue next update
+                return
+              end
+            end
+          end
+
         end
       end
     end
